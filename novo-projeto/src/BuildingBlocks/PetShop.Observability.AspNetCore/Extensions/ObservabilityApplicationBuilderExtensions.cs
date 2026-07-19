6dbe72872d4851e7ba812ae935b5b27c4c1f4ff3
Rodrigo de Oliveira <rodrigodotnet@gmail.com>
2026-07-18 23:56:10 -0300

    feat: add observability propagation libraries

---
+++ novo-projeto/src/BuildingBlocks/PetShop.Observability.AspNetCore/Extensions/ObservabilityApplicationBuilderExtensions.cs
@@ -0,0 +1,14 @@
+using PetShop.Observability.AspNetCore.Middlewares;
+
+namespace PetShop.Observability.AspNetCore.Extensions;
+
+public static class ObservabilityApplicationBuilderExtensions
+{
+    public static IApplicationBuilder UsePetShopObservabilityContext(
+        this IApplicationBuilder app)
+    {
+        ArgumentNullException.ThrowIfNull(app);
+
+        return app.UseMiddleware<CorrelationContextMiddleware>();
+    }
+}
