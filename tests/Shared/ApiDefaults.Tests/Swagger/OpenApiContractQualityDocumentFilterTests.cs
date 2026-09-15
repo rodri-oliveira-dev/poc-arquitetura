3ade49f2bb4e8a4aadb058acaefec97d089862a4
Rodrigo de Oliveira <rodrigodotnet@gmail.com>
2026-09-15 16:35:23 -0300

    test(openapi): use v3 media type interface collection

--- tests/Shared/ApiDefaults.Tests/Swagger/OpenApiContractQualityDocumentFilterTests.cs
+++ tests/Shared/ApiDefaults.Tests/Swagger/OpenApiContractQualityDocumentFilterTests.cs
@@ -157,7 +157,7 @@ public sealed class OpenApiContractQualityDocumentFilterTests
                     Schema = new OpenApiSchema { Type = JsonSchemaType.String }
                 }
             },
-            Content = new Dictionary<string, OpenApiMediaType>
+            Content = new Dictionary<string, IOpenApiMediaType>
             {
                 ["text/plain"] = new OpenApiMediaType
                 {
